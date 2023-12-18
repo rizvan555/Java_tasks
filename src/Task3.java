8098abaa8aec448a67f0ae71b6c0e513687b1eec
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-12-18 11:36:56 +0100

    Task about function in functions(return)

---
+++ src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    public static int multiplyToTwo(int a) {
+        return a * 2;
+    }
+
+    public static int additionToFive(int a) {
+        return a + 5;
+    }
+
+    public static int divisionToTwo (int a){
+        return a / 2;
+    }
+    public static void main(String[] args) {
+        System.out.println(multiplyToTwo(5));
+        System.out.println(additionToFive(10));
+        System.out.println(divisionToTwo(50));
+
+    }
+
+}
